7212c3b9d82002c0b06db67faf09d12abdba2e61
Alexey Zholtikov <git@zh.com.ru>
2024-02-09 08:10:16 +0300

    Minor changes

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 git clone http://git.zh.com.ru/alexey.zholtikov/zh_gateway.git
 cd zh_gateway
-mkdir components
-cd components
-git clone http://git.zh.com.ru/alexey.zholtikov/zh_config.git
-git clone http://git.zh.com.ru/alexey.zholtikov/zh_vector.git
-git clone -b esp32 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_network.git
-git clone http://git.zh.com.ru/alexey.zholtikov/zh_json.git
-cd ..
+git submodule init
+git submodule update
